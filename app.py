a5f3369fcb55dd6bdbffb32ffd554ef29a236971
Abhigyan Shanker <abhigyanshanker7@gmail.com>
2022-09-07 20:30:23 +0530

    updated app.py

--- app.py
+++ app.py
@@ -3,10 +3,15 @@ import transformers
 from transformers import AutoTokenizer, GPTJForCausalLM
 from flask import Flask, request, jsonify
 
+
 num_gpus = torch.cuda.device_count()
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-model_path = config.json["model_path"]
-tokenizer = config.json["tokenizer"]
+model_path = "./model"
+tokenizer = "EleutherAI/gpt-j-6b"
 
 model = GPTJForCausalLM.from_pretrained(model_path).to(device)
+tokenizer = AutoTokenizer.from_pretrained(tokenizer)
+
+
+
